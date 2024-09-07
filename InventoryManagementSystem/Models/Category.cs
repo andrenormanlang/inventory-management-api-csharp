3fd8b37f1dbe2260b44feab7d0d2eb800e70874c
andrenormanlang <andrernormanlang@gmail.com>
2024-09-07 22:25:56 +0200

    deploying api to azure

--- InventoryManagementSystem/Models/Category.cs
+++ InventoryManagementSystem/Models/Category.cs
@@ -3,7 +3,7 @@ namespace InventoryManagementSystem.Models
     public class Category
     {
         public int CategoryId { get; set; }
-        public string Name { get; set; }
+        public string? Name { get; set; }
         public List<Product> Products { get; set; } = new List<Product>();
     }
 }
--- InventoryManagementSystem/Models/Product.cs
+++ InventoryManagementSystem/Models/Product.cs
@@ -3,7 +3,7 @@ using InventoryManagementSystem.Models;
 public class Product
 {
     public int ProductId { get; set; }
-    public string Name { get; set; }
+    public string? Name { get; set; }
     public double Price { get; set; }
     public int Quantity { get; set; }
 
